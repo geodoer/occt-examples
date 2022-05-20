590c4327a6a0923aa3d2802a33c8bbf8a9639f46
geodoer <geodoer@outlook.com>
2022-05-20 13:43:05 +0800

    feat: Hi Occt Wth Conan
    Signed-off-by: geodoer <geodoer@outlook.com>

---
+++ A-UseOCCT/ConanOcct/src/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+#include <Standard_CString.hxx>
+
+int main(void)
+{
+   Standard_CString strHelloWorld("Hello World!");
+   Standard_CString strHelloOcct("Hello OpenCascade!");
+
+   std::cout << strHelloWorld << std::endl;
+   std::cout << strHelloOcct << std::endl;
+   
+   return 0;
+}
